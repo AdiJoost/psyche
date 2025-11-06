a9da0ba0d89775978920bb5f4e86e2529e779078
Adrian Joost <adi.joost@gmail.com>
2025-11-06 11:38:38 +0100

    other stuff

--- run.py
+++ run.py
@@ -1,9 +1,7 @@
 from src.zeroshots.run_zero_shot import run, run_suicide_watch
-from dotenv import load_dotenv
 
 def main():
     print("Application started")
-    load_dotenv()
     run_suicide_watch()
 
 if __name__ == "__main__":
